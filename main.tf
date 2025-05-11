26fb4de10892a62e06e2229bcfccc1d0bbe1ed15
santosh <santosh1808.singh@gmail.com>
2025-05-11 15:42:35 +0530

    New code

---
+++ main.tf
@@ -0,0 +1,116 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "ec49edde-0e94-4197-abdd-1d8d13b05b62"
+}
+
+# Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "linux-vm-rg"
+  location = "West US"
+}
+
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "linux-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "linux-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# NSG (Network Security Group)
+resource "azurerm_network_security_group" "nsg" {
+  name                = "linux-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+# Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "linux-vm-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+}
+
+# Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "linux-vm-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Linux Virtual Machine
+resource "azurerm_linux_virtual_machine" "linux_vm" {
+  name                            = "my-linux-vm"
+  resource_group_name             = azurerm_resource_group.rg.name
+  location                        = azurerm_resource_group.rg.location
+  size                            = "Standard_B1s"
+  admin_username                  = "santosh"
+  admin_password                  = "Santosh@1234"
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "linux-os-disk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_managed_disk" "external_disk" {
+  name                 = "external-data-disk"
+  location             = azurerm_resource_group.rg.location
+  resource_group_name  = azurerm_resource_group.rg.name
+  storage_account_type = "Standard_LRS"
+  create_option        = "Empty"
+  disk_size_gb         = 20
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "external_disk_attachment" {
+  managed_disk_id    = azurerm_managed_disk.external_disk.id
+  virtual_machine_id = azurerm_linux_virtual_machine.linux_vm.id
+  lun                = 0              # Logical Unit Number – start with 0
+  caching            = "ReadWrite"
+  create_option      = "Attach"
+}
